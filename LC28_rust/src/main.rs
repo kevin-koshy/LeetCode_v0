8f94affd6852ae7e100b1e97d59d26b2c83cdf9a
kevin-koshy <kevin_koshy@hotmail.com>
2023-10-26 13:55:37 -0400

    LC28

--- LC28_rust/src/main.rs
+++ LC28_rust/src/main.rs
@@ -1,14 +1,21 @@
 struct Solution;
 impl Solution {
-    pub fn str_str(haystack: String, needle: String) -> i32 {
-
-0
+    pub fn str_str(_haystack: String, _needle: String) -> i32 {
+        if &_haystack.len() < &_needle.len() {
+            return -1;
+        }
+        for i in 0.._haystack.len() - _needle.len() + 1 {
+            if _haystack[i..i + _needle.len()] == _needle {
+                return i as i32;
+            }
+        }
+        return -1;
 
     }
 }
 
 fn main() {
-    let string1 = String::from("sadbutsad");
-    let string2 = String::from("sad");
+    let string1 = String::from("xsadbutsad");
+    let string2 = String::from("sadsdafasdfasdfas");
     println!("{}", Solution::str_str(string1, string2));
 }
---
+++ LC28_rust/target/debug/deps/LC28_rust-bf57d5db886c0bf6.d
@@ -0,0 +1,5 @@
+/Users/kevinkoshy/Documents/Development/LeetCode/LC28_rust/target/debug/deps/LC28_rust-bf57d5db886c0bf6.rmeta: src/main.rs
+
+/Users/kevinkoshy/Documents/Development/LeetCode/LC28_rust/target/debug/deps/LC28_rust-bf57d5db886c0bf6.d: src/main.rs
+
+src/main.rs:
