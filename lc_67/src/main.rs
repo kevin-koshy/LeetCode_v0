bf0c5cbf0c594f765517bcaba5a36e7e4e27d160
kevin-koshy <kevin_koshy@hotmail.com>
2023-10-29 00:40:01 -0400

    LC_67 edge conditions not fixed

--- lc_67/src/main.rs
+++ lc_67/src/main.rs
@@ -12,9 +12,34 @@ impl Solution {
         println!("a:{}",a);
         println!("b:{}", b);
 
+        let mut count = char_width;
+        let mut result = String::new();
+        while count > 0 {
+            // let mut result = '0';
+            if a.chars().nth(count -1).unwrap() == '1'{
+                carry += 1;
+            }
+            if b.chars().nth(count -1).unwrap() == '1'{
+                carry += 1;
+            }
+            if carry % 2 == 1 {
+                result.push('1');
+            }
+            else {
+                result.push('0');
+            }
+            carry /= 2;
+            if carry == 1 {
+                result.push('0');
+            }
+            count -= 1;
+        }
 
 
-    "".to_string()
+        let result: String = result.chars().rev().collect();
+
+
+    result.to_string()
     }
 }
 
