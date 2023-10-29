742d4c63ee57648315d3e0eab62d9441bef6c904
kevin-koshy <kevin_koshy@hotmail.com>
2023-10-28 23:58:23 -0400

    LC67 update

--- lc_67/src/main.rs
+++ lc_67/src/main.rs
@@ -1,26 +1,27 @@
+use std::fmt::format;
+
 struct Solution;
 
 impl Solution {
     pub fn add_binary(a: String, b: String) -> String {
-        let mut a = a.chars().rev();
-        let mut b = b.chars().rev();
         let mut carry = 0;
-        let mut result = String::new();
-        while a.len() > 0 || b.len() > 0 || carry != 0 {
-            let mut sum = carry;
-            if a.len() > 0 {
+        let char_width = std::cmp::max(a.len(), b.len());
+        println!("char_width: {}", char_width);
+        let mut a = format!("{:0>char_width$}", a);
+        let mut b = format!("{:0>char_width$}", b);
+        println!("a:{}",a);
+        println!("b:{}", b);
 
-            }
-        }
-        result.chars().rev().collect()
 
+
+    "".to_string()
     }
 }
 
 
 fn main() {
     println!("Hello, world!");
-    let a = "11".to_string();
+    let a = "111111".to_string();
     let b = "1".to_string();
     let result = Solution::add_binary(a, b);
     println!("{}", result);
---
+++ lc_67/target/debug/lc_67.d
@@ -0,0 +1,1 @@
+/Users/kevinkoshy/Documents/Development/LeetCode/lc_67/target/debug/lc_67: /Users/kevinkoshy/Documents/Development/LeetCode/lc_67/src/main.rs
